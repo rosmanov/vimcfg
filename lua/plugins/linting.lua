05e39a6bd39601efcc0c6061bbf62ca4dd4130c5
Ruslan Osmanov <608192+rosmanov@users.noreply.github.com>
2026-01-28 14:11:08 +0000

    Increase timeout for linting

--- lua/plugins/linting.lua
+++ lua/plugins/linting.lua
@@ -96,7 +96,7 @@ return {
           if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
             return
           end
-          return { timeout_ms = 500, lsp_fallback = true }
+          return { timeout_ms = 3000, lsp_fallback = true }
         end,
       })
 
